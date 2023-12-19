f01500aeb55fe10ad267e57abcc0b73b563f1514
Askeran17 <>
2023-12-19 08:32:31 +0000

    write variabes for start screen and funktion

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,3 +3,17 @@
 
 // I set this option so JSHint doesn't raise unnecessary warnings.”
 
+//start screen
+let startPage = document.querySelector('.start-display');
+let startButton = document.getElementById('start-button');
+let quizContainer = document.getElementById('quiz');
+
+//when user click on start button
+startButton.addEventListener('click', () => {
+	startPage.classList.add('hide');
+	quizContainer.classList.remove('hide');
+  });
+
+  quizContainer.classList.add('quiz');
+
+  
